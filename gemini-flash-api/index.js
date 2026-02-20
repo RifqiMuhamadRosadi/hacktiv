b79943db52179d3cd35932e2ca5800c7c37bec7a
RifqiMuhamadRosadi <rifqimuhammm@gmail.com>
2026-02-20 08:37:20 +0700

    create endpoint1 (generate text)

--- gemini-flash-api/index.js
+++ gemini-flash-api/index.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import multer from "multer";
+import fs from "fs/promises";
 import { GoogleGenAI } from "@google/genai";
 
 const app = express();
@@ -12,6 +13,24 @@ const GEMINI_MODEL = "gemini-2.5-flash";
 app.use(express.json());
 
 const PORT = 3000;
+
+app.post("/generate-text", async (req, res) => {
+  const { prompt } = req.body;
+
+  try {
+    const response = await ai.models.generateContent({
+      model: GEMINI_MODEL,
+      contents: prompt,
+    });
+
+    res.status(200).json({ result: response.text });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: e.message
+    });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server ready on http://localhost:${PORT}`);
