e484826439e549a107140e31ef716d5413fbeeef
RifqiMuhamadRosadi <rifqimuhammm@gmail.com>
2026-02-20 10:50:48 +0700

    create endpoint3 (summary doc)

--- gemini-flash-api/index.js
+++ gemini-flash-api/index.js
@@ -53,6 +53,26 @@ app.post("/generate-from-image", upload.single("image"), async (req, res) => {
   }
 });
 
+app.post("/generate-from-document", upload.single("document"), async (req, res) => {
+    const { prompt } = req.body;
+    const base64Document = req.file.buffer.toString("base64");
+
+    try {
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents: [
+                { text: prompt ?? "Tolong buat ringkasan dari dokumen berikut.", type: "text" },
+                { inlineData: { data: base64Document, mimeType: req.file.mimetype } }
+            ],
+        });
+
+        res.status(200).json({ result: response.text });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ message: e.message });
+    }
+});
+
 app.listen(PORT, () => {
   console.log(`Server ready on http://localhost:${PORT}`);
 });
